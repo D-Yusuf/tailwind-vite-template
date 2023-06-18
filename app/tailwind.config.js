4fdb207a21cfe3be397130b45505ed769b984828
D-Yusuf <alnasiriyusuf@gmail.com>
2023-06-18 11:50:00 +0300

    Added the template

---
+++ app/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",
+  "./public/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
